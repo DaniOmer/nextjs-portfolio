3f5d3fce39ad25a0e18afc04652c8813729bda78
Omer Dotchamou <omer.dotchamou@scalens.com>
2023-11-06 10:21:29 +0100

    Adding booking website to work section

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,12 +1,21 @@
 import SectionTitle from "../SectionTitle/SectionTitle";
 import WorkCard from "./WorkCard";
 import WORK1 from "../../../public/images/work-1.png";
+import WORK2 from "../../../public/images/work-2.png";
 
 import { BiLogoReact, BiLogoTailwindCss } from "react-icons/bi";
+import { SiFirebase, SiMui } from "react-icons/si";
 import { DiSass } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
 
 const Works = () => {
-  const languages = [<BiLogoReact />, <BiLogoTailwindCss />, <DiSass />];
+  const languages1 = [<BiLogoReact />, <BiLogoTailwindCss />, <DiSass />];
+  const languages2 = [
+    <TbBrandNextjs />,
+    <SiFirebase />,
+    <BiLogoTailwindCss />,
+    <SiMui />,
+  ];
   return (
     <section className="mt-4 pt-20 md:pt-32" id="works">
       <SectionTitle title="Works" />
@@ -16,28 +25,28 @@ const Works = () => {
           title="Agency website"
           description="A gracefully designed website for a dynamic communication agency. A perfect blend of visual creativity and engaging content."
           preview="https://cts-benin.com/"
-          languages={languages}
+          languages={languages1}
+        />
+        <WorkCard
+          image={WORK2}
+          title="Booking website"
+          description="A sophisticated booking website tailored for scheduling appointments and services at a hair salon."
+          preview="https://charme-coiffure.vercel.app/"
+          languages={languages2}
         />
         <WorkCard
           image={WORK1}
           title="Agency website"
           description="A gracefully designed website for a dynamic communication agency. A perfect blend of visual creativity and engaging content."
           preview="https://cts-benin.com/"
-          languages={languages}
+          languages={languages1}
         />
         <WorkCard
           image={WORK1}
           title="Agency website"
           description="A gracefully designed website for a dynamic communication agency. A perfect blend of visual creativity and engaging content."
           preview="https://cts-benin.com/"
-          languages={languages}
-        />
-        <WorkCard
-          image={WORK1}
-          title="Agency website"
-          description="A gracefully designed website for a dynamic communication agency. A perfect blend of visual creativity and engaging content."
-          preview="https://cts-benin.com/"
-          languages={languages}
+          languages={languages1}
         />
       </div>
     </section>
